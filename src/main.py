94e976c1c29ac57a117dad808f4889a84654cd28
Emilio-Zuniga <zzze616@gmail.com>
2026-04-22 00:57:38 +0000

    It's come to this --  one last Hail Mary

--- src/main.py
+++ src/main.py
@@ -3,18 +3,25 @@ import time
 
 def main():
     # Initialize connection to drone
-    drone = drone_controller('udp:127.0.0.1:15551')
+    time.sleep(30)
+    #drone = drone_controller('udp:127.0.0.1:15551')
+    drone = drone_controller('/dev/ttyACM0', baud=115200)
 
     # Takeoff to 10 meters
     drone.set_mode('GUIDED')
-    drone.takeoff(10)
+    drone.takeoff(2)
 
+    time.sleep(5)
+    for i in range(12):
+        drone.rotate(angle=30, relative=True)
+        time.sleep(2)
     '''
     # We're square dancing with this one, so let's do a little twirl
     for i in range(4):
         drone.move(forward=30)
         drone.rotate(angle=90, relative=True)
     '''
+    time.sleep(5)
     
     # Land drone
     drone.land()
